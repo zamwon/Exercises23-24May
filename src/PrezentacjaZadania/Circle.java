5e82fe5255815ead702eb2cfe799c51c83eea5be
zamwon <blazej.karnecki@gmail.com>
2020-05-23 11:32:00 +0200

    Add Circle implementation

---
+++ src/PrezentacjaZadania/Circle.java
@@ -0,0 +1,38 @@
+package PrezentacjaZadania;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
+class Circle {
+
+
+    private static final double PI_DOUBLE = Math.PI;
+    private final float radius;
+    private static final float PI = 3.14f;
+    Circle(float diameter){
+        this.radius = diameter / 2;
+    }
+    public float getCircumference(){
+        return 2* PI * radius;
+    }
+    public double getCircumferenceDouble(){
+        return 2* PI_DOUBLE * radius;
+    }
+
+    public float getArea() {
+        return PI * radius * radius;
+
+    }
+    public double getAreaDouble() {
+        return PI_DOUBLE * radius * radius;
+
+    }
+
+    public void testVarags(String powitanie, int... values){
+        System.out.println(powitanie);
+
+        for (int inaczej : values) {
+            System.out.println(inaczej);
+        }
+    }
+}
