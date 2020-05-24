1d166742301d1300734a9642fac5940fcdd734ef
zamwon <blazej.karnecki@gmail.com>
2020-05-24 15:20:52 +0200

    add Ex8Method

---
+++ src/PrezentacjaZadania/ex8/Ex8Method.java
@@ -0,0 +1,18 @@
+package PrezentacjaZadania.ex8;
+
+import java.util.Scanner;
+
+public class Ex8Method {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj pierwszą liczbę");
+        int pierwszaLiczba = scanner.nextInt();
+        System.out.println("Podaj drugą liczbę");
+        int drugaLiczba = scanner.nextInt();
+        System.out.println("Podaj działanie");
+        String dzialanie = scanner.next();
+        Kalkulator kalkulator = new Kalkulator();
+        float wynik = kalkulator.wykonajDzialanie(pierwszaLiczba, drugaLiczba, dzialanie);
+        System.out.println(wynik);
+    }
+}
