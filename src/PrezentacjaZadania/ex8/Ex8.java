826781d3f638e5783c834d0c6b7923abf86e11eb
zamwon <blazej.karnecki@gmail.com>
2020-05-24 15:07:58 +0200

    add class Kalkulator, edit Ex8 into OOP approach

--- src/PrezentacjaZadania/ex8/Ex8.java
+++ src/PrezentacjaZadania/ex8/Ex8.java
@@ -3,43 +3,17 @@ package PrezentacjaZadania.ex8;
 import java.util.Scanner;
 
 public class Ex8 {
-    private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-
-        System.out.println("Podaj liczbę");
-        final float liczba1 = scanner.nextFloat();
-
-        System.out.println("Jakie działanie chcesz wykonać :");
-        System.out.println("+ by dodawać");
-        System.out.println("- by odejmować");
-        System.out.println("* by pomnożyć");
-        System.out.println("/ by podzielić");
-        String dzialanie = scanner.next();
-
-        final float liczba2 = scanner.nextFloat();
-
-        switch (dzialanie) {
-            case "+":
-                System.out.println(liczba1 + liczba2);
-                break;
-            case "-":
-                System.out.println(liczba1 - liczba2);
-                break;
-            case "*":
-                System.out.println((double) liczba1 * (double) liczba2);
-                break;
-            case "/":
-                if (liczba2 == 0) {
-                    System.out.println("Błąd - nie dziel przez 0");
-                    break;
-                } else {
-                    System.out.println((double) liczba1 / (double) liczba2);
-                    break;
-                }
-            default:
-                System.out.println("Wprowadzono niepoprawne działanie - Błędny znak");
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Podaj pierwszą liczbę");
+            int pierwszaLiczba = scanner.nextInt();
+            System.out.println("Podaj drugą liczbę");
+            int drugaLiczba = scanner.nextInt();
+            System.out.println("Podaj działanie");
+            String dzialanie = scanner.next();
+            Kalkulator kalkulator = new Kalkulator();
+            float wynik = kalkulator.wykonajDzialanie(pierwszaLiczba, drugaLiczba, dzialanie);
+            System.out.println(wynik);
         }
-
     }
-}
---
+++ src/PrezentacjaZadania/ex8/Kalkulator.java
@@ -0,0 +1,35 @@
+package PrezentacjaZadania.ex8;
+
+public class Kalkulator {
+    public int dodaj(int pierwszaLiczba, int drugaLiczba) {
+        return pierwszaLiczba + drugaLiczba;
+    }
+    public int odejmij(int pierwszaLiczba, int drugaLiczba) {
+        return pierwszaLiczba - drugaLiczba;
+    }
+    public int pomnoz(int pierwszaLiczba, int drugaLiczba) {
+        return pierwszaLiczba * drugaLiczba;
+    }
+    public float podziel(int pierwszaLiczba, int drugaLiczba) {
+        if (drugaLiczba == 0) {
+            System.out.println("Nie dzielimy przez 0");
+            return 0;
+        }
+        return ((float) pierwszaLiczba) / drugaLiczba;
+    }
+    public float wykonajDzialanie(int pierwszaLiczba, int drugaLiczba, String dzialanie) {
+        switch (dzialanie) {
+            case "+":
+                return dodaj(pierwszaLiczba, drugaLiczba);
+            case "-":
+                return odejmij(pierwszaLiczba, drugaLiczba);
+            case "*":
+                return pomnoz(pierwszaLiczba, drugaLiczba);
+            case "/":
+                return podziel(pierwszaLiczba, drugaLiczba);
+            default:
+                System.out.println("Wprowadzono błędny znak");
+        }
+        return 0;
+    }
+}
